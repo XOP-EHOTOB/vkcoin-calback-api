be4bdf39c7d8da7fdfccf8fc64fbcfe6e4aff049
XOP-EHOTOB <dima.dimitrev@gmail.com>
2022-10-06 20:23:38 +0300

    4.0.0

--- index.js
+++ index.js
@@ -1,143 +1,96 @@
-const request = require("prequest");
 const http = require("http");
-
-let port = 1234;
+const axios = require("axios");
+const md5 = require('js-md5');
+let pollingPort;
 
 module.exports = class VKCOIN {
-  constructor(auth) {
+  constructor (auth) {
     this.key = auth.key;
     this.id = auth.id;
   }
 
-  async startServer(props) {
-    if (!props.server)
-      return console.error( "Start server Error: не указан сервер для получения событий!");
-    if (props.port) { port = props.port;}
+  async startServer (props) {
+    if (!props.server) throw new Error("Ошибка запуска сервера: не указан сервер для получения событий!");
+    pollingPort = props.port || 1234;
     try {
-      let connect = await request({
+      return (await axios({
         method: "POST",
         url: "https://coin-without-bugs.vkforms.ru/merchant/set/",
-        form: {
-          callback: `http://${props.server}:${port}`,
-          merchantId: this.id,
-          key: this.key,
-        },
-        headers: { "Content-type": "Content-Type: application/json" },
-      });
-      return connect
-    } catch (e) {
-      throw new Error(e.message || "Ошибка сервера!")
-    }
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify({ callback: `http://${props.server}:${pollingPort}`, merchantId: this.id, key: this.key })
+      })).data;
+    } catch (e) { throw new Error(e.message || "Ошибка сервера!"); }
   }
 
-  async startPolling(hand) {
+  async startPolling (hand) {
     try {
       http.createServer((req, res) => {
-          if (req.method === "POST") {
-            let body = "";
-            req.on("data", (chunk) => {
-              body += chunk.toString();
-            });
-            req.on("end", () => {
-              let event = JSON.parse(body);
-              res.writeHead(200, "OK");
-              res.end("OK");
-              hand(event);
-            });
-          }
-        }).listen(port);
-    } catch (err) {
-      throw new Error(e.message || "Ошибка сервера!")
-    }
+        if (req.method === "POST") {
+          let body = "";
+          req.on("data", chunk => {
+            body += chunk.toString();
+            if(body.length > 1e6) {
+              req.connection.destroy();
+              body = "";
+            };
+          });
+          req.on("end", () => {
+            const event = JSON.parse(body);
+            const key = md5(event.id + ';' + event.fromId + ';' + event.amount + ';' + event.payload + ';' + this.key);
+            if(event.key !== key) {
+              req.connection.destroy();
+              return;
+            };
+            res.writeHead(200, "OK");
+            res.end("OK");
+            hand(event);
+          });
+        }
+      }).listen(pollingPort);
+    } catch (e) { throw new Error(e.message || "Ошибка сервера!"); }
   }
 
-  async send(toId, amount, fromShop) {
-    if (!fromShop) {
-      try {
-        let req = await request({
-          method: "POST",
-          url: "https://coin-without-bugs.vkforms.ru/merchant/send/",
-          form: {
-            merchantId: this.id,
-            key: this.key,
-            toId: toId,
-            amount: amount,
-          },
-          headers: { "Content-type": "Content-Type: application/json" },
-        });
-        return req
-      } catch (e) {
-        console.error(e.message);
-        throw new Error(e.message || "Ошибка сервера!")
-      }
-    } else {
-      try {
-        let req = await request({
-          method: "POST",
-          url: "https://coin-without-bugs.vkforms.ru/merchant/send/",
-          form: {
-            merchantId: this.id,
-            key: this.key,
-            toId: toId,
-            amount: amount,
-            markAsMerchant: fromShop,
-          },
-          headers: { "Content-type": "Content-Type: application/json" },
-        });
-        return req
-      } catch (e) {
-        throw new Error(e.message || "Ошибка сервера!")
-      }
-    }
-  }
-
-  async getBalance(id) {
+  async send (toId, amount, fromShop) {
+    const dataJSON = fromShop ? { merchantId: this.id, key: this.key, toId, amount, markAsMerchant: true } : { merchantId: this.id, key: this.key, toId, amount };
     try {
-      let balance = await request({
+      return (await axios({
+        method: "POST",
+        url: "https://coin-without-bugs.vkforms.ru/merchant/send/",
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify(dataJSON)
+      })).data;
+    } catch (e) { throw new Error(e.message || "Ошибка сервера!"); }
+  }
+
+  async getBalance (id) {
+    try {
+      id = Array.isArray(id) ? id : [id];
+      if (id.length > 100) throw new Error("Ошибка: Длина массива должна быть не более 100!");
+      return (await axios({
         method: "POST",
         url: "https://coin-without-bugs.vkforms.ru/merchant/score/",
-        form: {
-          merchantId: this.id,
-          key: this.key,
-          userIds: id,
-        },
-        headers: { "Content-type": "Content-Type: application/json" },
-      });
-      return balance;
-    } catch (e) {
-      throw new Error(e.message || "Ошибка сервера!")
-    }
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify({ merchantId: this.id, key: this.key, userIds: id })
+      })).data;
+    } catch (e) { throw new Error(e.message || "Ошибка сервера!"); }
   }
 
-  async shopName(name) {
+  async shopName (name) {
     try {
-      let req = await request({
+      return (await axios({
         method: "POST",
         url: "https://coin-without-bugs.vkforms.ru/merchant/set/",
-        form: {
-          merchantId: this.id,
-          key: this.key,
-          name: name,
-        },
-        headers: { "Content-type": "Content-Type: application/json" }
-      });
-      return req
-    } catch (e) {
-      throw new Error(e.message || "Ошибка сервера!")
-    }
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify({ merchantId: this.id, key: this.key, name })
+      })).data;
+    } catch (e) { throw new Error(e.message || "Ошибка сервера!"); }
   }
 
-  getLink(amount, payload, fixed) {
-    if (!this.id)
-      throw new Error(`Ошибка: Для начала необходимо авторизироваться!`)
-    if (isNaN(+amount))
-      throw new Error(`Ошибка: Недопустимый формат VK Coin!`)
-    let link = `vk.com/coin#x${this.id}_${+amount}_${payload}`
-    if (fixed) { link += "_1" }
-    return link
+  getLink (amount, payload, isFixed) {
+    if (!this.id) throw new Error("Ошибка: Для начала необходимо авторизироваться!");
+    if (isNaN(Number(amount))) throw new Error("Ошибка: Недопустимый формат VK Coin!");
+    return `vk.com/coin#x${this.id}_${Number(amount)}_${payload}${isFixed ? "" : "_1"}`;
   }
 
-  format(amount) {
-    return (amount / 1000).toLocaleString('de-DE').replace(/\./g, " ")
-  }
+  format (amount) { return (Number(amount) / 1000).toLocaleString("ru-RU"); }
 }
